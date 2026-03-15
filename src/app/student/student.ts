ad1395bc7bbae6fc65410493b9e2fcc130a719c2
Baiju Achari <baijuachari2025@gmail.com>
2026-03-14 21:21:18 -0400

    change search varaible name

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-student',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './student.html',
+  styleUrl: './student.scss',
 })
-export class StudentComponent {}
+export class StudentComponent {
+  lastNameQuery = '';
+
+  onSearch() {
+    console.log('Searching for:', this.lastNameQuery);
+  }
+}
